3db965c8320c8966a650e300a66ddc02b78b60d7
Alex Remar <takoe.moscow@gmail.com>
2023-08-17 12:12:58 +0300

    initial commit

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-import PopoverUi from './PopoverUi';
-import PopoverController from './PopoverController';
-
-const popoverUi = new PopoverUi();
-popoverUi.bindToDOM(document.querySelector('#container'));
-
-const popoverCtrl = new PopoverController(popoverUi);
-popoverCtrl.init();
